765138ecc45496f504cb5a134e08e19374d4615b
FreeApp2014 <admin@imac.local>
2020-08-06 18:24:56 +0300

    Initial Commit

---
+++ malugri-mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  malugri-mobile
+//
+//  Created by admin on 06/08/2020.
+//  Copyright © 2020 FreeAppSW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
